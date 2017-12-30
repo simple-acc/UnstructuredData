a73825436bb893e749d4a83422c997c495fe8096
MT-Lin <mingtielin@outlook.com>
2017-12-30 23:28:47 +0800

    Initial commit

---
+++ src/main/java/com/lmt/data/unstructured/UnstructuredDataApplication.java
@@ -0,0 +1,12 @@
+package com.lmt.data.unstructured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnstructuredDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnstructuredDataApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lmt/data/unstructured/UnstructuredDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lmt.data.unstructured;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnstructuredDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
